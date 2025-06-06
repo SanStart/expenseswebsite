25c2040c8c436f52153d9261cbf813880be524cd
Your Name <your.email@example.com>
2025-06-06 14:49:27 +0300

    Add Render domain to ALLOWED_HOSTS

--- expenseswebsite/settings.py
+++ expenseswebsite/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = 'django-insecure-mw64eml22bt_-=n+f+zlvli)cx*3o@h%%p7q!d((3-^b#rb!@r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
